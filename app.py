510759e0c15f9429a231576cff6fc75e78ff5e30
Dion Maber <dionm@outlook.com>
2019-06-19 11:27:30 +1000

    updates for PIM role reporting

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import uuid
 import requests
 import config
 
+from flask import jsonify
+
+
 app = flask.Flask(__name__)
 app.debug = True
 app.secret_key = 'development'
@@ -46,14 +49,12 @@ def main_logic():
     if state != flask.session['state']:
         raise ValueError("State does not match")
     auth_context = adal.AuthenticationContext(AUTHORITY_URL)
-    token_response = auth_context.acquire_token_with_authorization_code(code, REDIRECT_URI, config.RESOURCE,
-                                                                        config.CLIENT_ID, config.CLIENT_SECRET)
+    token_response = auth_context.acquire_token_with_authorization_code(code, REDIRECT_URI, config.RESOURCE, config.CLIENT_ID, config.CLIENT_SECRET)
     # It is recommended to save this to a database when using a production app.
     flask.session['access_token'] = token_response['accessToken']
-
     return flask.redirect('/graphcall')
 
-
+'''
 @app.route('/graphcall')
 def graphcall():
     if 'access_token' not in flask.session:
@@ -66,7 +67,93 @@ def graphcall():
                     'client-request-id': str(uuid.uuid4())}
     graph_data = requests.get(endpoint, headers=http_headers, stream=False).json()
     return flask.render_template('display_graph_info.html', graph_data=graph_data)
+'''
 
+@app.route('/graphcall')
+def graphcall():
+    if 'access_token' not in flask.session:
+        return flask.redirect(flask.url_for('login'))
+    
+    print ('Bearer ' + flask.session.get('access_token'))
+    
+    http_headers = {'Authorization': 'Bearer ' + flask.session.get('access_token'),
+                    'User-Agent': 'adal-python-sample',
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'client-request-id': str(uuid.uuid4())}
+
+
+    # build lookup table for privileged roles
+    # get roles
+    #priv_roles = config.RESOURCE + '/' + config.API_VERSION + "/privilegedRoles?$filter=startswith(name,'Glob')"
+    priv_roles = config.RESOURCE + '/' + config.API_VERSION + '/privilegedRoles'
+    
+    graph_data_priv_roles = requests.get(priv_roles, headers=http_headers, stream=False).json()
+
+    for role in graph_data_priv_roles['value']:
+        print(".. processing : " + role["name"])
+        role.update( {'role_members' : []} )
+
+        # get priv role members
+        priv_role_members = config.RESOURCE + '/' + config.API_VERSION + '/privilegedRoles/' + role["id"] + '/assignments'
+        graph_data_priv_role_members = requests.get(priv_role_members, headers=http_headers, stream=False).json()
+
+        for priv_user in graph_data_priv_role_members['value']:
+            this_user = get_aad_user(priv_user["userId"], http_headers)
+
+            role["role_members"].append(
+                {
+                    "displayName": this_user["displayName"],
+                    "userPrincipalName": this_user["userPrincipalName"],
+                    "accountEnabled": this_user["accountEnabled"],
+                    "employeeId": this_user["employeeId"],
+                    "id": this_user["id"],
+                    "jobTitle": this_user["jobTitle"],
+                    "department": this_user["department"],
+                    "onPremisesSyncEnabled": this_user["onPremisesSyncEnabled"]
+                })
+ 
+    return jsonify(graph_data_priv_roles)
+
+    #return flask.render_template('display_nested_graph_info.html', graph_data_priv_roles=graph_data_priv_roles)
+    #return jsonify(graph_data_priv_roles)
+
+
+    #endpoint = config.RESOURCE + '/' + config.API_VERSION + '/me/'
+    #graph_data = requests.get(endpoint, headers=http_headers, stream=False).json()
+    #return graph_data
+    #return flask.render_template('display_graph_info.html', graph_data=graph_data)
+
+
+def get_aad_user(userId, http_headers):
+    if 'access_token' not in flask.session:
+        return flask.redirect(flask.url_for('login'))
+    
+    print('   --> aad user lookup: ' + userId)
+    endpoint_aad_user = config.RESOURCE + '/' + config.API_VERSION + '/users/' + userId
+    graph_data_aad_user = requests.get(endpoint_aad_user, headers=http_headers, stream=False).json()
+
+    return graph_data_aad_user
+
+
+'''
+@app.route('/graphcall')
+def graphcall():
+    if 'access_token' not in flask.session:
+        return flask.redirect(flask.url_for('login'))
+    #endpoint = config.RESOURCE + '/' + 'beta' + '/privilegedRoles/'
+    endpoint = config.RESOURCE + '/' + config.API_VERSION + '/me/'
+    http_headers = {'Authorization': 'Bearer ' + flask.session.get('access_token'),
+                    'User-Agent': 'adal-python-sample',
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'client-request-id': str(uuid.uuid4())}
+    graph_data = requests.get(endpoint, headers=http_headers, stream=False).json()
+    print (graph_data)
+    #return flask.render_template('display_graph_info.html', graph_data=graph_data)
+'''
+
+#def ()
 
 if __name__ == "__main__":
     app.run()
